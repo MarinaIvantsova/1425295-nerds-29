2abcbb5ec28c63a79543c8ff56f67e91743eb86d
Marina Ivantsova <marineivantsova@yandex.ru>
2020-11-10 23:43:35 +0300

    Анимация

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,9 @@ const openLink = document.querySelector(".feedback-popup");
 const closeLink = document.querySelector(".modal-close");
 
 const feedbackPopup = document.querySelector(".modal-feedback");
+const loginForm = document.querySelector(".login-form");
+const loginLogin = loginPopup.querySelector(".login-icon-name");
+const loginEmail = loginPopup.querySelector(".login-icon-email");
 
 
 openLink.addEventListener("click", function (evt) {
@@ -10,6 +13,18 @@ openLink.addEventListener("click", function (evt) {
 });
 
 closeLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
   feedbackPopup.classList.add("popup-close");
+  loginPopup.classList.remove("popup-open");
+  loginPopup.classList.remove("modal-error");
+});
+
+loginForm.addEventListener("submit", function (evt) {
+  if (!loginLogin.value || !loginEmail.value) {
+    evt.preventDefault();
+    feedbackPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", loginLogin.value);
+    }
+  }
 });
